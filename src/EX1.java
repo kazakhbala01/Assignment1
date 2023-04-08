bc795a23a4d5d8cf0eb654df22af7dfb611e81e2
Almas <amnbyalms@gmail.com>
2023-04-08 20:59:00 +0600

    Exercise 1 with documentation.

--- src/EX1.java
+++ src/EX1.java
@@ -1,24 +1,34 @@
-import java.util.Scanner;
-public class EX1 {
-    public void sol(){
-        System.out.println("Write number of elements and elements\n For example 5\n 1 2 4 5 6");
-        Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        int[] arr= new int[a];
-        for (int x=0;x<a;x++){
-            arr[x]=sc.nextInt();
-        }
-        System.out.println(FindMin(arr,a));
-        }
+/**
+ * Find the minimum number of given array
+ * @param arr is an integer array
+ * @param n is the number of elements in the array
+ * @return the minimum number in the array
+ */
 
-    public static int FindMin(int[] arr,int n){
+import java.util.Scanner;
+
+public class EX1 {
+
+    public static int FindMin(int[] arr,int n) {
         int min;
-        if(n==1){
-            return arr[0];
+        if (n == 1) {
+            return arr[0]; //If n equals to 1, returns last element of array, recursion ends
         }
-        else{
-            min=FindMin(arr,n-1);
+        else {
+            min = FindMin(arr, n-1); //recurse next function with n-1
         }
-        return Math.min(arr[n-1],min);
+        return Math.min(arr[n-1], min); //Compare both numbers and returns minimum of them
+    }
+
+    public static void sol(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt(); //number of elements in array
+        int[] arr = new int[n]; //creating integer array
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt(); //input elements into array
+        }
+        System.out.println("The minimum number in the array is " + FindMin(arr, n)); //Print output of the function FindMin
     }
 }
